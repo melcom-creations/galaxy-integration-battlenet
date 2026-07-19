1406b7872a886977bb151611a2147be915dc1bfb
melcom <melcom@gmx.net>
2026-07-19 04:56:42 +0200

    Delete old root source files

--- __init__.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-# Add the bundled Modules directory to sys.path so local dependencies can be imported.
-modules_dir = os.path.join(os.path.dirname(__file__), 'Modules')
-if os.path.isdir(modules_dir) and modules_dir not in sys.path:
-    sys.path.insert(0, modules_dir)
